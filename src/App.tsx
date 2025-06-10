508136fc5399b0b063cebdc2039e8dafb41d0d81
jonathan choi <jchoi4497@gmail.com>
2025-06-09 20:49:37 -0700

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -31,16 +31,23 @@ function App() {
     });
   };
 
+  const handlePlaceClick = (place: Place) => {
+    setPlace(place);
+
+    document.getElementById('map-section')?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+
   return (
     <div className="flex flex-col md:flex-row gap-6">
       <div className="w-full md:w-1/3">
         <LocationSearch
-          onPlaceClick={(p) => setPlace(p)}
+          onPlaceClick={handlePlaceClick}
           darkMode={darkMode}
           onToggleDarkMode={onToggleDarkMode}
         />
       </div>
-      <div className="w-full md:w-2/3">
+      <div id="map-section" className="w-full md:w-2/3">
         <Map place={place} />
       </div>
     </div>
